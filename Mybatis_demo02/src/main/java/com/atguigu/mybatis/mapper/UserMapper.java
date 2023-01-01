ed8358aa314f4ad28c4565b4ba55e918625bca97
xionghu <1796235969@qq.com>
2023-01-01 18:38:57 +0800

    Add mybatis: 设置mybatis-config.xml模板

---
+++ Mybatis_demo02/src/main/java/com/atguigu/mybatis/mapper/UserMapper.java
@@ -0,0 +1,11 @@
+package com.atguigu.mybatis.mapper;
+
+/**
+ * @Description:
+ * @Author: xionghu514
+ * @Date: 2022/12/31 16:22
+ * @Email: 1796235969@qq.com
+ */
+public interface UserMapper {
+
+}
---
+++ Mybatis_demo02/src/main/java/com/atguigu/mybatis/pojo/User.java
@@ -0,0 +1,89 @@
+package com.atguigu.mybatis.pojo;
+
+/**
+ * @Description:
+ * @Author: xionghu514
+ * @Date: 2022/12/31 16:18
+ * @Email: 1796235969@qq.com
+ */
+public class User {
+    private Integer id;
+    private String username;
+    private String password;
+    private Integer age;
+    private String sex;
+    private String email;
+
+    public User() {
+    }
+
+    public User(Integer id, String username, String password, Integer age, String sex, String email) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.age = age;
+        this.sex = sex;
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getusername() {
+        return username;
+    }
+
+    public void setusername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", age=" + age +
+                ", sex='" + sex + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
