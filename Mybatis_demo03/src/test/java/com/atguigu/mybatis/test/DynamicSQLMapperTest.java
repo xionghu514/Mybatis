bc763d9b90cb7cdaa3ce308a8e547b84edf6188b
xionghu <1796235969@qq.com>
2023-01-02 23:51:04 +0800

    Add mybatis: 动态SQL之 where 标签

--- Mybatis_demo03/src/test/java/com/atguigu/mybatis/test/DynamicSQLMapperTest.java
+++ Mybatis_demo03/src/test/java/com/atguigu/mybatis/test/DynamicSQLMapperTest.java
@@ -13,10 +13,19 @@ import org.junit.Test;
  * @Email: 1796235969@qq.com
  */
 public class DynamicSQLMapperTest {
+
+    /**
+     *  动态sql标签：
+     *     1. if：根据标签中test的表达式决定sql语句是否拼接
+     *     2. where：
+     *          where标签有内容时，会自动生成where关键字，并且将内容前的 and 或者 or 删掉
+     *          如过where标签没有内容，where将不生效
+     *          注意：where只能删除内容前的and 或者 or，内容后的 and 或者 or无法删除
+     */
     @Test
     public void testQueryEmpByCondition() {
         SqlSession sqlSession = SqlSessionUtils.getSqlSession();
         DynamicSQLMapper mapper = sqlSession.getMapper(DynamicSQLMapper.class);
-        System.out.println(mapper.queryEmpByCondition(new Emp(null, "张三", 12, "男", "1232@qq.com", null)));
+        System.out.println(mapper.queryEmpByCondition(new Emp(null, null, null, "男", null, null)));
     }
 }
