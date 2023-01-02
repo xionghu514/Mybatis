90f843ce769f532a4d7339faf57105bdcea72560
xionghu <1796235969@qq.com>
2023-01-02 14:00:36 +0800

    Add mybatis: mybatis的模糊查询

---
+++ Mybatis_demo02/src/main/java/com/atguigu/mybatis/mapper/SQLMapper.java
@@ -0,0 +1,16 @@
+package com.atguigu.mybatis.mapper;
+
+import com.atguigu.mybatis.pojo.User;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+/**
+ * @Description:
+ * @Author: xionghu514
+ * @Date: 2023/1/2 13:48
+ * @Email: 1796235969@qq.com
+ */
+public interface SQLMapper {
+    List<User> queryUserByLike(@Param("username") String username);
+}
---
+++ Mybatis_demo02/src/test/java/com/atguigu/mybatis/test/SQLMapperTest.java
@@ -0,0 +1,25 @@
+package com.atguigu.mybatis.test;
+
+import com.atguigu.mybatis.mapper.SQLMapper;
+import com.atguigu.mybatis.pojo.User;
+import com.atguigu.mybatis.utils.SqlSessionUtils;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+import java.util.List;
+
+/**
+ * @Description:
+ * @Author: xionghu514
+ * @Date: 2023/1/2 13:51
+ * @Email: 1796235969@qq.com
+ */
+public class SQLMapperTest {
+    @Test
+    public void testQueryUserByLike() {
+        SqlSession sqlSession = SqlSessionUtils.getSqlSession();
+        SQLMapper mapper = sqlSession.getMapper(SQLMapper.class);
+        List<User> users = mapper.queryUserByLike("zhang");
+        System.out.println(users);
+    }
+}
